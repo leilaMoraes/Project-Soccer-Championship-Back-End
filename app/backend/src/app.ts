a2a434733ace2ac1851ffc4314933e8510aec097
Leila Moraes <leila.violino@gmail.com>
2023-03-30 16:23:41 -0300

    feat: adiciona middleware de erro

--- app/backend/src/app.ts
+++ app/backend/src/app.ts
@@ -1,5 +1,7 @@
 import * as express from 'express';
 import teamsRouter from './database/routes/TeamsRoutes';
+import 'express-async-errors';
+import errorMiddleware from './database/middlewares/error';
 
 class App {
   public app: express.Express;
@@ -24,6 +26,7 @@ class App {
     this.app.use(express.json());
     this.app.use(accessControl);
     this.app.use('/teams', teamsRouter);
+    this.app.use(errorMiddleware);
   }
 
   public start(PORT: string | number):void {
---
+++ app/backend/src/database/middlewares/error.ts
@@ -0,0 +1,15 @@
+import { NextFunction, Request, Response } from 'express';
+import ApiError from '../utils/ApiError';
+
+const errorMiddleware = (
+  error: Error & Partial<ApiError>,
+  _req: Request,
+  res: Response,
+  _next: NextFunction,
+) => {
+  console.log('err', error);
+  const { status, message } = error;
+  res.status(status || 500).json({ message });
+};
+
+export default errorMiddleware;
