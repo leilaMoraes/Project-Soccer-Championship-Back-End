bd9d0d8182c568962907a3a39cad726215b11f1e
Leila Moraes <leila.violino@gmail.com>
2023-04-04 22:31:02 -0300

    test: adiciona teste para as partidas filtradas

--- app/backend/src/tests/matches.test.ts
+++ app/backend/src/tests/matches.test.ts
@@ -14,16 +14,32 @@ chai.use(chaiHttp);
 const { expect } = chai;
 
 describe('Testa rota /matches', () => {
-   let chaiHttpResponse: Response;
+  let chaiHttpResponse: Response;
 
-   afterEach(()=>{
-     sinon.restore();
-   })
+  afterEach(()=>{
+    sinon.restore();
+  })
 
-   it('1) Testa a rota /matches com o findAll', async () => {
+  it('1) Testa a rota /matches com o findAll', async () => {
     sinon.stub(MatchesModel, 'findAll').resolves(matches)
-     chaiHttpResponse = await chai.request(app).get('/matches');
-     expect(chaiHttpResponse.status).to.be.equal(200);
-     expect(chaiHttpResponse.body).to.be.deep.equal(matches);
+    chaiHttpResponse = await chai.request(app).get('/matches');
+    expect(chaiHttpResponse.status).to.be.equal(200);
+    expect(chaiHttpResponse.body).to.be.deep.equal(matches);
+  });
+
+  it('2) Testa a rota /matches?inProgress=false', async () => {
+    const filterFalse = matches.filter((m: { inProgress: string; }) => m.inProgress === 'false')
+    sinon.stub(MatchesModel, 'findAll').resolves(filterFalse)
+    chaiHttpResponse = await chai.request(app).get('/matches?inProgress=false');
+    expect(chaiHttpResponse.status).to.be.equal(200);
+    expect(chaiHttpResponse.body).to.be.deep.equal(filterFalse);
+  });
+
+  it('3) Testa a rota /matches?inProgress=true', async () => {
+    const filterTrue = matches.filter((m: { inProgress: string; }) => m.inProgress === 'true')
+    sinon.stub(MatchesModel, 'findAll').resolves(filterTrue)
+    chaiHttpResponse = await chai.request(app).get('/matches?inProgress=true');
+    expect(chaiHttpResponse.status).to.be.equal(200);
+    expect(chaiHttpResponse.body).to.be.deep.equal(filterTrue);
   });
 });
--- app/backend/src/tests/teams.test.ts
+++ app/backend/src/tests/teams.test.ts
@@ -14,23 +14,23 @@ chai.use(chaiHttp);
 const { expect } = chai;
 
 describe('Testa rota /teams', () => {
-   let chaiHttpResponse: Response;
+  let chaiHttpResponse: Response;
 
-   afterEach(()=>{
-     sinon.restore();
-   })
+  afterEach(()=>{
+    sinon.restore();
+  })
 
-   it('1) Testa a rota /teams com o findAll', async () => {
+  it('1) Testa a rota /teams com o findAll', async () => {
     sinon.stub(TeamsModel, 'findAll').resolves(teams)
-     chaiHttpResponse = await chai.request(app).get('/teams');
-     expect(chaiHttpResponse.status).to.be.equal(200);
-     expect(chaiHttpResponse.body).to.be.deep.equal(teams);
+    chaiHttpResponse = await chai.request(app).get('/teams');
+    expect(chaiHttpResponse.status).to.be.equal(200);
+    expect(chaiHttpResponse.body).to.be.deep.equal(teams);
   });
 
   it('2) Testa a rota /teams/:id com o findByPk', async () => {
     sinon.stub(TeamsModel, 'findByPk').resolves(teams[1])
-     chaiHttpResponse = await chai.request(app).get('/teams/2');
-     expect(chaiHttpResponse.status).to.be.equal(200);
-     expect(chaiHttpResponse.body).to.be.deep.equal(teams[1]);
+    chaiHttpResponse = await chai.request(app).get('/teams/2');
+    expect(chaiHttpResponse.status).to.be.equal(200);
+    expect(chaiHttpResponse.body).to.be.deep.equal(teams[1]);
   });
 });
