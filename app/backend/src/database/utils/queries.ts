f7b4a9ce81dd5251b46b721988a1c2134c2f0fb1
Leila Moraes <leila.violino@gmail.com>
2023-04-12 21:05:11 -0300

    feat: endpoint /leaderboard/home acrescenta goalsBalance e Efficiency

--- app/backend/src/database/utils/queries.ts
+++ app/backend/src/database/utils/queries.ts
@@ -6,12 +6,18 @@ COUNT(CASE WHEN m.home_team_goals > m.away_team_goals THEN 1 ELSE NULL END) AS t
 COUNT(CASE WHEN m.home_team_goals = m.away_team_goals THEN 1 ELSE NULL END) AS totalDraws,
 COUNT(CASE WHEN m.home_team_goals < m.away_team_goals THEN 1 ELSE NULL END) AS totalLosses,
 SUM(home_team_goals) AS goalsFavor,
-SUM(away_team_goals) AS goalsOwn
+SUM(away_team_goals) AS goalsOwn,
+SUM(home_team_goals) - SUM(away_team_goals) AS goalsBalance,
+ROUND(
+  (SUM
+    (CASE WHEN m.home_team_goals > m.away_team_goals THEN 3
+      WHEN m.home_team_goals < m.away_team_goals THEN 0 ELSE 1 END) / 
+      (COUNT(*) * 3.0)) * 100, 2) AS efficiency
 FROM TRYBE_FUTEBOL_CLUBE.teams AS t
 INNER JOIN TRYBE_FUTEBOL_CLUBE.matches AS m
 ON m.home_team_id = t.id
 WHERE m.in_progress IS false
 GROUP BY name
-ORDER BY totalPoints DESC, totalVictories DESC`;
+ORDER BY totalPoints DESC, totalVictories DESC, goalsBalance DESC, goalsFavor DESC;`;
 
 export default home;
